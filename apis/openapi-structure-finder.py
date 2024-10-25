41bae23939c08a73ebdde632611f4336d6806c2b
Sara Eraso <sara.eraso@correounivalle.edu.co>
2024-10-25 16:45:04 +0200

    add apis files

--- apis/openapi-structure-finder.py
+++ apis/openapi-structure-finder.py
@@ -1,6 +1,6 @@
 import json
 
-with open("apis/marvel/marvel-openapi.json", "r", encoding="utf-8") as openfile:
+with open("apis/yelp/yelp-openapi.json", "r", encoding="utf-8") as openfile:
     dic = json.load(openfile)
 
 structureDict = {
