1e57096acb9f17ca6614e9a196ce20baf3df9f53
alixdecr <alix.decrop@unamur.be>
2024-10-30 12:51:02 +0100

    added digdag api doc

--- apis/openapi-structure-finder.py
+++ apis/openapi-structure-finder.py
@@ -1,6 +1,6 @@
 import json
 
-with open("apis/management-apache-cassandra/management-apache-cassandra-openapi.json", "r", encoding="utf-8") as openfile:
+with open("apis/digdag/digdag-openapi.json", "r", encoding="utf-8") as openfile:
     dic = json.load(openfile)
 
 structureDict = {
