2a6937d40216a39e72c6cfe60fa5fa7844cfee73
alixdecr <alix.decrop@unamur.be>
2024-11-05 15:41:26 +0100

    removed user management api as ref is not defined

--- apis/openapi-structure-finder.py
+++ apis/openapi-structure-finder.py
@@ -1,6 +1,6 @@
 import json
 
-with open("apis/openverse/openverse-openapi.json", "r", encoding="utf-8") as openfile:
+with open("apis/senzing/senzing-openapi.json", "r", encoding="utf-8") as openfile:
     dic = json.load(openfile)
 
 structureDict = {
