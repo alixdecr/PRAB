f58b988ec99866b42951d76859f1b1f551a01477
alixdecr <alix.decrop@unamur.be>
2024-11-07 17:26:35 +0100

    added structural characteristics recap

--- apis/openapi-structure-finder.py
+++ apis/openapi-structure-finder.py
@@ -1,6 +1,6 @@
 import json
 
-with open("apis/marvel/marvel-openapi.json", "r", encoding="utf-8") as openfile:
+with open("apis/ocvn/ocvn-openapi.json", "r", encoding="utf-8") as openfile:
     dic = json.load(openfile)
 
 structureDict = {
